544ba11196401ccad24bc6b193aa59bdc33a2fa9
cesar <cesar@CesarJZB>
2023-01-27 14:35:02 -0600

    CesarVistas6

--- src/main/java/cesar/gestionAcademica4/GestionAcademica.java
+++ src/main/java/cesar/gestionAcademica4/GestionAcademica.java
@@ -20,12 +20,15 @@ public class GestionAcademica extends javax.swing.JDialog {
     public GestionAcademica(java.awt.Frame parent, boolean modal) {
         super(parent, modal);
         initComponents();
-        //this.txtBuscar.requestFocus();
+        t=new TablaGestion();
+        this.jTable1.setModel(t);
         this.btnEditar.setVisible(false);
-       this.btnEliminar.setVisible(false);
-       this.txtOtro.setVisible(false);
-       t=new TablaGestion();
-       this.jTable1.setModel(t);
+        this.btnEliminar.setVisible(false);
+        //this.txtBuscar.requestFocus();
+        
+       
+       //t=new TablaGestion();
+       //this.jTable1.setModel(t);
        bd=new BDActividades();
        LinkedList<Gestion> lista=bd.obtener();
        for(int i=0;i<lista.size();i++){
@@ -43,23 +46,12 @@ public class GestionAcademica extends javax.swing.JDialog {
     private void initComponents() {
 
         jPanel1 = new javax.swing.JPanel();
-        txtComision = new javax.swing.JTextField();
-        btnBuscar = new javax.swing.JButton();
-        jLabel1 = new javax.swing.JLabel();
-        jLabel2 = new javax.swing.JLabel();
-        txtBuscar = new javax.swing.JTextField();
-        jLabel4 = new javax.swing.JLabel();
-        jLabel5 = new javax.swing.JLabel();
-        jLabel6 = new javax.swing.JLabel();
         btnAgregar = new javax.swing.JButton();
         btnEditar = new javax.swing.JButton();
         btnEliminar = new javax.swing.JButton();
-        btnSalir = new javax.swing.JButton();
-        jcomboActividad = new javax.swing.JComboBox<>();
-        jdateInicio = new com.toedter.calendar.JDateChooser();
-        jdateFin = new com.toedter.calendar.JDateChooser();
-        txtOtro = new javax.swing.JTextField();
+        btnGuardar = new javax.swing.JButton();
         btnPdf = new javax.swing.JButton();
+        btnSalir = new javax.swing.JButton();
         jScrollPane2 = new javax.swing.JScrollPane();
         jTable1 = new javax.swing.JTable();
 
@@ -70,49 +62,6 @@ public class GestionAcademica extends javax.swing.JDialog {
         jPanel1.setBorder(javax.swing.BorderFactory.createLineBorder(new java.awt.Color(0, 0, 0)));
         jPanel1.setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
 
-        txtComision.addKeyListener(new java.awt.event.KeyAdapter() {
-            public void keyReleased(java.awt.event.KeyEvent evt) {
-                txtComisionKeyReleased(evt);
-            }
-        });
-        jPanel1.add(txtComision, new org.netbeans.lib.awtextra.AbsoluteConstraints(100, 110, 130, 30));
-
-        btnBuscar.setFont(new java.awt.Font("SansSerif", 0, 12)); // NOI18N
-        btnBuscar.setText("Buscar");
-        btnBuscar.addMouseListener(new java.awt.event.MouseAdapter() {
-            public void mouseClicked(java.awt.event.MouseEvent evt) {
-                btnBuscarMouseClicked(evt);
-            }
-        });
-        jPanel1.add(btnBuscar, new org.netbeans.lib.awtextra.AbsoluteConstraints(10, 0, 80, 25));
-
-        jLabel1.setFont(new java.awt.Font("SansSerif", 0, 12)); // NOI18N
-        jLabel1.setText("Actividad:");
-        jPanel1.add(jLabel1, new org.netbeans.lib.awtextra.AbsoluteConstraints(10, 60, -1, -1));
-
-        jLabel2.setFont(new java.awt.Font("SansSerif", 0, 12)); // NOI18N
-        jLabel2.setText("Comisión o puesto:");
-        jPanel1.add(jLabel2, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, 110, -1, -1));
-
-        txtBuscar.addKeyListener(new java.awt.event.KeyAdapter() {
-            public void keyReleased(java.awt.event.KeyEvent evt) {
-                txtBuscarKeyReleased(evt);
-            }
-        });
-        jPanel1.add(txtBuscar, new org.netbeans.lib.awtextra.AbsoluteConstraints(90, 0, 140, 30));
-
-        jLabel4.setFont(new java.awt.Font("SansSerif", 0, 12)); // NOI18N
-        jLabel4.setText("Periodo:");
-        jPanel1.add(jLabel4, new org.netbeans.lib.awtextra.AbsoluteConstraints(10, 130, -1, -1));
-
-        jLabel5.setFont(new java.awt.Font("SansSerif", 0, 12)); // NOI18N
-        jLabel5.setText("Inicio:");
-        jPanel1.add(jLabel5, new org.netbeans.lib.awtextra.AbsoluteConstraints(10, 150, -1, -1));
-
-        jLabel6.setFont(new java.awt.Font("SansSerif", 0, 12)); // NOI18N
-        jLabel6.setText("Fin:");
-        jPanel1.add(jLabel6, new org.netbeans.lib.awtextra.AbsoluteConstraints(10, 180, -1, -1));
-
         btnAgregar.setFont(new java.awt.Font("SansSerif", 0, 12)); // NOI18N
         btnAgregar.setText("Agregar");
         btnAgregar.addMouseListener(new java.awt.event.MouseAdapter() {
@@ -120,7 +69,7 @@ public class GestionAcademica extends javax.swing.JDialog {
                 btnAgregarMouseClicked(evt);
             }
         });
-        jPanel1.add(btnAgregar, new org.netbeans.lib.awtextra.AbsoluteConstraints(270, 10, 80, 25));
+        jPanel1.add(btnAgregar, new org.netbeans.lib.awtextra.AbsoluteConstraints(60, 20, 80, 30));
 
         btnEditar.setFont(new java.awt.Font("SansSerif", 0, 12)); // NOI18N
         btnEditar.setText("Editar");
@@ -129,7 +78,7 @@ public class GestionAcademica extends javax.swing.JDialog {
                 btnEditarMouseClicked(evt);
             }
         });
-        jPanel1.add(btnEditar, new org.netbeans.lib.awtextra.AbsoluteConstraints(270, 40, 80, 25));
+        jPanel1.add(btnEditar, new org.netbeans.lib.awtextra.AbsoluteConstraints(350, 20, 80, 30));
 
         btnEliminar.setFont(new java.awt.Font("SansSerif", 0, 12)); // NOI18N
         btnEliminar.setText("Eliminar");
@@ -138,33 +87,29 @@ public class GestionAcademica extends javax.swing.JDialog {
                 btnEliminarMouseClicked(evt);
             }
         });
-        jPanel1.add(btnEliminar, new org.netbeans.lib.awtextra.AbsoluteConstraints(270, 70, 80, 25));
+        jPanel1.add(btnEliminar, new org.netbeans.lib.awtextra.AbsoluteConstraints(200, 20, 80, 30));
 
-        btnSalir.setFont(new java.awt.Font("SansSerif", 0, 12)); // NOI18N
-        btnSalir.setText("Salir y Guardar");
+        btnGuardar.setFont(new java.awt.Font("SansSerif", 0, 12)); // NOI18N
+        btnGuardar.setText("Guardar");
+        btnGuardar.addMouseListener(new java.awt.event.MouseAdapter() {
+            public void mouseClicked(java.awt.event.MouseEvent evt) {
+                btnGuardarMouseClicked(evt);
+            }
+        });
+        jPanel1.add(btnGuardar, new org.netbeans.lib.awtextra.AbsoluteConstraints(200, 90, 80, 30));
+
+        btnPdf.setText("Crear PDF");
+        jPanel1.add(btnPdf, new org.netbeans.lib.awtextra.AbsoluteConstraints(200, 150, 90, 30));
+
+        btnSalir.setText("Salir");
         btnSalir.addMouseListener(new java.awt.event.MouseAdapter() {
             public void mouseClicked(java.awt.event.MouseEvent evt) {
                 btnSalirMouseClicked(evt);
             }
         });
-        jPanel1.add(btnSalir, new org.netbeans.lib.awtextra.AbsoluteConstraints(260, 130, 120, 25));
+        jPanel1.add(btnSalir, new org.netbeans.lib.awtextra.AbsoluteConstraints(360, 150, 80, 30));
 
-        jcomboActividad.setFont(new java.awt.Font("SansSerif", 0, 12)); // NOI18N
-        jcomboActividad.setModel(new javax.swing.DefaultComboBoxModel<>(new String[] { "Radio", "Conferencia", "Foro", "Viaje", "otro" }));
-        jcomboActividad.addActionListener(new java.awt.event.ActionListener() {
-            public void actionPerformed(java.awt.event.ActionEvent evt) {
-                jcomboActividadActionPerformed(evt);
-            }
-        });
-        jPanel1.add(jcomboActividad, new org.netbeans.lib.awtextra.AbsoluteConstraints(60, 40, 170, 30));
-        jPanel1.add(jdateInicio, new org.netbeans.lib.awtextra.AbsoluteConstraints(40, 150, 120, -1));
-        jPanel1.add(jdateFin, new org.netbeans.lib.awtextra.AbsoluteConstraints(30, 180, 130, -1));
-        jPanel1.add(txtOtro, new org.netbeans.lib.awtextra.AbsoluteConstraints(60, 70, 170, -1));
-
-        btnPdf.setText("Crear PDF");
-        jPanel1.add(btnPdf, new org.netbeans.lib.awtextra.AbsoluteConstraints(270, 180, -1, -1));
-
-        getContentPane().add(jPanel1, new org.netbeans.lib.awtextra.AbsoluteConstraints(150, 380, 390, 220));
+        getContentPane().add(jPanel1, new org.netbeans.lib.awtextra.AbsoluteConstraints(30, 380, 610, 220));
 
         jTable1.setModel(new javax.swing.table.DefaultTableModel(
             new Object [][] {
@@ -177,6 +122,11 @@ public class GestionAcademica extends javax.swing.JDialog {
                 "Title 1", "Title 2", "Title 3", "Title 4"
             }
         ));
+        jTable1.addMouseListener(new java.awt.event.MouseAdapter() {
+            public void mouseClicked(java.awt.event.MouseEvent evt) {
+                jTable1MouseClicked(evt);
+            }
+        });
         jScrollPane2.setViewportView(jTable1);
 
         getContentPane().add(jScrollPane2, new org.netbeans.lib.awtextra.AbsoluteConstraints(10, 10, 650, 360));
@@ -184,81 +134,44 @@ public class GestionAcademica extends javax.swing.JDialog {
         pack();
     }// </editor-fold>//GEN-END:initComponents
 
-    private void btnSalirMouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_btnSalirMouseClicked
-         ListaGestion actividades=t.getActividades();
+    private void btnGuardarMouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_btnGuardarMouseClicked
+        bd.borrar();
+        ListaGestion actividades=t.getActividades();
         for(int i=0;i<actividades.size();i++){
             bd.registrarActividad(actividades.getActividad(i));
         }
-        this.dispose();
-    }//GEN-LAST:event_btnSalirMouseClicked
-
-    private void txtComisionKeyReleased(java.awt.event.KeyEvent evt) {//GEN-FIRST:event_txtComisionKeyReleased
-       
-    }//GEN-LAST:event_txtComisionKeyReleased
+        
+    }//GEN-LAST:event_btnGuardarMouseClicked
 
     private void btnEditarMouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_btnEditarMouseClicked
         
     }//GEN-LAST:event_btnEditarMouseClicked
 
     private void btnEliminarMouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_btnEliminarMouseClicked
-        
+           int n=this.jTable1.getSelectedRow();
+           t.eliminarActividad(n);
+           
     }//GEN-LAST:event_btnEliminarMouseClicked
 
-    private void btnBuscarMouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_btnBuscarMouseClicked
-
-    }//GEN-LAST:event_btnBuscarMouseClicked
-
-    private void txtBuscarKeyReleased(java.awt.event.KeyEvent evt) {//GEN-FIRST:event_txtBuscarKeyReleased
-        if(!txtBuscar.getText().isEmpty()){
-            btnEditar.setVisible(true);
-            btnEliminar.setVisible(true);
-        }else{
-            btnEditar.setVisible(false);
-            btnEliminar.setVisible(false);
-        }
-    }//GEN-LAST:event_txtBuscarKeyReleased
-
     private void btnAgregarMouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_btnAgregarMouseClicked
-        //String actividad=this.jcomboActividad.getItemAt(WIDTH);
-        if(!this.txtOtro.isVisible()){
-            String actividad = jcomboActividad.getSelectedItem().toString();
-            String comision = this.txtComision.getText();
-            String dia = Integer.toString(jdateInicio.getCalendar().get(Calendar.DAY_OF_MONTH));
-            String mes = Integer.toString(jdateInicio.getCalendar().get(Calendar.MONTH) + 1);
-            String year = Integer.toString(jdateInicio.getCalendar().get(Calendar.YEAR));
-            String fecha = (dia + "/" + mes + "/" + year);
-            String dia1 = Integer.toString(jdateFin.getCalendar().get(Calendar.DAY_OF_MONTH));
-            String mes1 = Integer.toString(jdateFin.getCalendar().get(Calendar.MONTH) + 1);
-            String year1 = Integer.toString(jdateFin.getCalendar().get(Calendar.YEAR));
-            String fecha2 = (dia1 + "/" + mes1 + "/" + year1);
-            String periodo = "Inicio:" + fecha + "     " + "Fin:" + fecha2;
-            t.agregarActividad(new Gestion(actividad, comision, periodo));
-        }else{
-            String actividad2 = this.txtOtro.getText();
-            String comision = this.txtComision.getText();
-            String dia = Integer.toString(jdateInicio.getCalendar().get(Calendar.DAY_OF_MONTH));
-            String mes = Integer.toString(jdateInicio.getCalendar().get(Calendar.MONTH) + 1);
-            String year = Integer.toString(jdateInicio.getCalendar().get(Calendar.YEAR));
-            String fecha = (dia + "/" + mes + "/" + year);
-            String dia1 = Integer.toString(jdateFin.getCalendar().get(Calendar.DAY_OF_MONTH));
-            String mes1 = Integer.toString(jdateFin.getCalendar().get(Calendar.MONTH) + 1);
-            String year1 = Integer.toString(jdateFin.getCalendar().get(Calendar.YEAR));
-            String fecha2 = (dia1 + "/" + mes1 + "/" + year1);
-            String periodo = "Inicio:" + fecha + "     " + "Fin:" + fecha2;
-            t.agregarActividad(new Gestion(actividad2, comision, periodo));
+        FrmRegistrar act=new FrmRegistrar(null,true);  
+        act.setVisible(true);
+        Gestion a=act.getT();
+        if(a!=null){
+            t.agregarActividad(a);
         }
         
     }//GEN-LAST:event_btnAgregarMouseClicked
 
-    private void jcomboActividadActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jcomboActividadActionPerformed
-        if(this.jcomboActividad.getSelectedItem().toString().compareToIgnoreCase("otro")==0){
-            this.txtOtro.setVisible(true);
-            this.jcomboActividad.setVisible(false);
-        }else{
-             this.txtOtro.setVisible(false);
-             this.jcomboActividad.setVisible(true);
-         }
-    }//GEN-LAST:event_jcomboActividadActionPerformed
+    private void btnSalirMouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_btnSalirMouseClicked
+       this.dispose();
+    }//GEN-LAST:event_btnSalirMouseClicked
+
+    private void jTable1MouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_jTable1MouseClicked
+        this.btnEliminar.setVisible(true);
+        this.btnEditar.setVisible(true);
+        
+    }//GEN-LAST:event_jTable1MouseClicked
 
     /**
      * @param args the command line arguments
@@ -304,24 +217,13 @@ public class GestionAcademica extends javax.swing.JDialog {
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JButton btnAgregar;
-    private javax.swing.JButton btnBuscar;
     private javax.swing.JButton btnEditar;
     private javax.swing.JButton btnEliminar;
+    private javax.swing.JButton btnGuardar;
     private javax.swing.JButton btnPdf;
     private javax.swing.JButton btnSalir;
-    private javax.swing.JLabel jLabel1;
-    private javax.swing.JLabel jLabel2;
-    private javax.swing.JLabel jLabel4;
-    private javax.swing.JLabel jLabel5;
-    private javax.swing.JLabel jLabel6;
     private javax.swing.JPanel jPanel1;
     private javax.swing.JScrollPane jScrollPane2;
     private javax.swing.JTable jTable1;
-    private javax.swing.JComboBox<String> jcomboActividad;
-    private com.toedter.calendar.JDateChooser jdateFin;
-    private com.toedter.calendar.JDateChooser jdateInicio;
-    private javax.swing.JTextField txtBuscar;
-    private javax.swing.JTextField txtComision;
-    private javax.swing.JTextField txtOtro;
     // End of variables declaration//GEN-END:variables
 }
