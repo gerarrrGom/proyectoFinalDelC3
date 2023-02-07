f934f9b440fe8d9cbd7a309b9a64e94a23cab887
CarlosRxD <94030901+CarlosRxD@users.noreply.github.com>
2023-02-07 11:59:36 -0600

    Carlos R Cardoza R

---
+++ src/main/java/docencia/TablaPdf.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package docencia;
+
+
+import com.itextpdf.text.pdf.PdfPTable;
+
+/**
+ *
+ * @author cardo
+ */
+public class TablaPdf {
+    private PdfPTable tabla;
+}
